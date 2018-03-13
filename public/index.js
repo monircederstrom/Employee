17518e7c5e18c8fa924d2e9000824627eb447c6f
Monir Cederstrom <monircederstrom@bootcamp.local>
2018-03-13 18:06:07 -0500

    fix js and html bugs

--- public/index.js
+++ public/index.js
@@ -1,50 +1,68 @@
+console.log("working")
 var config = {
-  apiKey: "AIzaSyAqKWcRP3jSGDhUASkQcTLhXDKt2DgsRxE",
-  authDomain: "employee-d9cbf.firebaseapp.com",
-  databaseURL: "https://employee-d9cbf.firebaseio.com",
-  projectId: "employee-d9cbf",
-  storageBucket: "employee-d9cbf.appspot.com",
-  messagingSenderId: "887525033865"
-};
+  apiKey: "AIzaSyBK67aPNaZB212T_ELxC7jsjAF0dlQ2Woc",
+  authDomain: "dailyschedule-4c1c0.firebaseapp.com",
+  databaseURL: "https://dailyschedule-4c1c0.firebaseio.com",
+   projectId: "dailyschedule-4c1c0",
+   storageBucket: "dailyschedule-4c1c0.appspot.com",
+   messagingSenderId: "837111990737",
+}
 firebase.initializeApp(config);
-var employeeData = firebase.database();
+var database = firebase.database();
 $("#add-employee-btn").on("click", function() {
+	event.preventDefault();
 // Grabs user input
-var employeeName = $("#name-input").val().trim();
-var role = $("#role-input").val().trim();
-var startDate = $("#startDate-input").val().trim();
-var rate = $("#rate-input").val().trim();
+var employeeName = $("#name").val().trim();
+var role = $("#role").val().trim();
+var startDate = moment($("#startdate").val().trim(), "DD/MM/YY").format("X");
+var rate = $("#rate").val().trim();
 // Creates local object for holding EMployee data
 var newEmployee = {
 name: employeeName,
 role: role,
-startDate: startDate,
-rate: rate
+start: startDate,
+rate: rate,
 };
 // Uploads EMployee data to the database
-employeeData.ref().push(newEmployee);
+database.ref().push(newEmployee);
 // confirm in console
 console.log(newEmployee.name);
-console.log(newEMployee.role);
-console.log(newEMployee.startDate);
+console.log(newEmployee.role);
+console.log(newEMployee.start);
 console.log(newEMployee.rate);
 // Alert
 alert("New Employee Successfully Created");
 // Clears all of the text-boxes
-$("#name-input").val("");
-$("#role-input").val("");
-$("#first-input").val("");
-$("#rate-input").val("");
+$("#name").val("");
+$("#role").val("");
+$("#startdate").val("");
+$("#rate").val("");
 // Determine when the next EMployee arrives.
-return Employee;
 });
+
 //  Firebase event for adding EMployees to the database 
 //  and a row in the html when a user adds an entry
-EMployeeData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 console.log(childSnapshot.val());
 // EMployee app variables
 var tName = childSnapshot.val().name;
 var trole = childSnapshot.val().role;
 var trate = childSnapshot.val().rate;
-var tstartDate = childstartDate().startDate;
+var tstartDate = childSnapshot.val().start;
+
+console.log(tName);
+console.log(trole);
+console.log(trate);
+console.log(tstartDate);
+
+//moment.js
+var employeeStart = moment.unix(tstartDate).format("MM/DD/YY");
+var months = moment().diff(moment.unix(tstartDate, "X"), "months");
+console.log(months);
+
+//total billed rate
+var billed = months * trate;
+console.log(billed);
+$("#table > tbody").append("<tr><td>" + tName + "</td><td>" + trole + "</td><td>" +
+  employeeStart + "</td><td>" + months + "</td><td>" + trate + "</td><td>" + billed + "</td></tr>");
 });
